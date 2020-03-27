7d68c672ec5b0fa160fdd6f7b9db8c716ce8c59c
Santhosh <sandskies@gmail.com>
2020-03-27 13:44:52 -0700

    Xylophone sounds and app icon with functionality added using AVFoundation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        playSound(sound: sender.currentTitle!)
+                    
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
+    
+    var player: AVAudioPlayer?
 
-
+    func playSound(sound: String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav", subdirectory: "Sounds")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+    }
 }
 
